18f345cd16e98f4694073bcae60e25e772ef9709
fy <fy77645@126.com>
2024-08-06 17:54:54 +0800

    接口调试

--- src/api/product.js
+++ src/api/product.js
@@ -2,5 +2,9 @@ import { RestApi } from './rest-api'
 
 export const packageApi = new RestApi('/product/package/package')
 export const packageCategoryApi = new RestApi('/product/package/category')
+
+export const productCategoryApi = new RestApi('/product/category')
+export const productApi = new RestApi('/product/info')
+
 export const productGuideApi = new RestApi('/product/guide/guide')
 export const productGuideItemApi = new RestApi('/product/guide/item')
